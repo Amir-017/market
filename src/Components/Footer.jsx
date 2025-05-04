1fc9493bafc75e06f255efac0cf44ffa6230970d
Your Name <you@example.com>
2025-05-04 17:50:05 +0300

    la commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="px-10 dark:bg-blue-gray-800 bg-green-900 flex w-full flex-row  items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
       <Typography color="blue-gray" className="font-bold text-white">
-        &copy; 2023 Material Tailwind
+        &copy; 2023 MARKET
       </Typography>
       <ul className="flex flex-wrap items-center gap-y-2 gap-x-8 ">
         <li>
